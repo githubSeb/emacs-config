175d038d7f975c8933890ff4c417144ff253b3b5
ouabde_r <romain.ouabdelkader@gmail.com>
2014-12-05 10:57:31 +0100

    Remove powerline from default module

--- src/default-modules.el
+++ src/default-modules.el
@@ -12,7 +12,6 @@
 (conf:load-module "highlight-symbol")
 (conf:load-module "ido")
 (conf:load-module "multiple-cursors")
-(conf:load-module "powerline")
 (conf:load-module "smooth-scroll")
 (conf:load-module "tags")
 (conf:load-module "undo-tree")
