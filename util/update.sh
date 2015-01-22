54ab5100f7d86e46b2596262a225c17ea6bcd783
ouabde_r <romain.ouabdelkader@gmail.com>
2015-01-22 18:52:46 +0100

    Upgrade: use current branch instead of master branch

--- util/update.sh
+++ util/update.sh
@@ -2,7 +2,7 @@
 
 git fetch
 
-if [ `git rev-list HEAD...origin/master --count` != 0 ]
+if [ `git rev-list HEAD...$(git rev-parse --abbrev-ref --symbolic-full-name @{u}) --count` != 0 ]
 then
     git pull && ./install && echo 'Please restart emacs to apply changes'
 else
